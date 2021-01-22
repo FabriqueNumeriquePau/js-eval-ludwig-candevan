72ae6bf6278910fbaebbe1a29f897bea17d255c0
ludwig-candevan <ludwig.candevan@gmail.com>
2021-01-22 12:16:02 +0100

    envoi

--- js/dojosjs.js
+++ js/dojosjs.js
@@ -1,1 +1,36 @@
-console.log("Go Chuck !");
+// console.log("Go Chuck !");
+// document.querySelector("div");
+//     //La souris entre dans le aside//
+//     ("div").mouseenter(function(){
+//         //L'image devient carré//
+//         $(this).css("  border-radius: 0%;");
+//     });
+
+//     //La souris quitte le aside //
+//     ("div").mouseleave(function(){
+//         //l'image reprend sa forme initiale//
+//         $(this).css(" border-radius: 50%;");
+        
+
+//     });
+// });
+
+let chuck = querySelector("div");
+
+function changeImg(){
+    this.dojojs.border-radius= 0%
+}
+
+chuck.addEventListener("mouseover",changeImg);
+});
+
+let chuck = querySelector("div");
+
+function changeImg(){
+    this.dojojs.border-radius= 50%
+}
+
+chuck.addEventListener("mouseout",changeImg);
+});
+
+
